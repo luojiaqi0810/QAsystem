015d1226cf649400aa5f971b82f1f65821188547
birdegg <313993678@qq.com>
2019-10-18 20:53:44 +0800

    add lrange method

--- src/main/java/com/nowcoder/wenda/util/JedisAdapter.java
+++ src/main/java/com/nowcoder/wenda/util/JedisAdapter.java
@@ -391,6 +391,20 @@ public class JedisAdapter implements InitializingBean {
         return null;
     }
 
+    public List<String> lrange(String key, long start,long end) {
+        Jedis jedis = null;
+        try {
+            jedis = pool.getResource();
+            return jedis.lrange(key, start, end);
+        } catch (Exception e) {
+            LOGGER.error("lrange方法发生异常：" + e.getMessage());
+        }finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+        return null;
+    }
 
 
 }
