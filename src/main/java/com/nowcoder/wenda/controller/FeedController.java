3260438e0c91bad309a99a9bc32ae392ef2a124a
birdegg <313993678@qq.com>
2019-10-18 20:55:19 +0800

    get feeds from redis by "push model"

--- src/main/java/com/nowcoder/wenda/controller/FeedController.java
+++ src/main/java/com/nowcoder/wenda/controller/FeedController.java
@@ -5,6 +5,8 @@ import com.nowcoder.wenda.model.Feed;
 import com.nowcoder.wenda.model.HostHolder;
 import com.nowcoder.wenda.service.FeedService;
 import com.nowcoder.wenda.service.FollowService;
+import com.nowcoder.wenda.util.JedisAdapter;
+import com.nowcoder.wenda.util.RedisKeyUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +34,27 @@ public class FeedController {
     HostHolder hostHolder;
     @Autowired
     FollowService followService;
+    @Autowired
+    JedisAdapter jedisAdapter;
+
+
+    @RequestMapping(path = {"/pushfeeds"}, method = {RequestMethod.GET, RequestMethod.POST})
+    public String getPushFeeds(Model model) {
+        //如果没登陆，就取0
+        int localUserId = hostHolder.getUser() == null ? 0 : hostHolder.getUser().getId();
+
+        List<String> feedIds = jedisAdapter.lrange(RedisKeyUtil.getTimelineKey(localUserId), 0, 10);
+        List<Feed> feeds = new ArrayList<>();
+        for (String feedId : feedIds) {
+            Feed feed = feedService.getById(Integer.parseInt(feedId));
+            if (feed == null) {
+                continue;
+            }
+            feeds.add(feed);
+        }
+        model.addAttribute("feeds", feeds);
+        return "feeds";
+    }
 
     @RequestMapping(path = {"/pullfeeds"}, method = {RequestMethod.GET, RequestMethod.POST})
     public String getPullFeeds(Model model) {
